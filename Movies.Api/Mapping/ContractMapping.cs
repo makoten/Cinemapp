6209b37d12a5df6e051b2a52a97a8ac161af9e70
Nathan Dimitriades <makoten@proton.me>
2024-09-15 20:46:20 +0300

    chore: add endpoint mapping

--- Movies.Api/Mapping/ContractMapping.cs
+++ Movies.Api/Mapping/ContractMapping.cs
@@ -1,5 +1,6 @@
 ﻿using Movies.Application.Models;
 using Movies.Contracts.Requests;
+using Movies.Contracts.Responses;
 
 namespace Movies.Api.Mapping
 {
@@ -13,6 +14,25 @@ namespace Movies.Api.Mapping
                 Title = request.Title,
                 YearOfRelease = request.YearOfRelease,
                 Genres = request.Genres.ToList()
+            };
+        }
+
+        public static MovieResponse MapToResponse(this Movie movie)
+        {
+            return new MovieResponse
+            {
+                Id = movie.Id,
+                Title = movie.Title,
+                YearOfRelease = movie.YearOfRelease,
+                Genres = movie.Genres
+            };
+        }
+
+        public static MoviesResponse MapToResponse(this IEnumerable<Movie> movies)
+        {
+            return new MoviesResponse
+            {
+                Items = movies.Select(MapToResponse)
             };
         }
     }
