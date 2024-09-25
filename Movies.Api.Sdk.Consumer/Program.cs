12de4f6fe49ce7f9eb79079a9712ccbea5a01d8b
Nathan Dimitriades <makoten@proton.me>
2024-09-25 22:02:08 +0300

    Add 2 GET movie endpoints to the SDK

--- Movies.Api.Sdk.Consumer/Program.cs
+++ Movies.Api.Sdk.Consumer/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using System.Text.Json;
+using Movies.Api.Sdk;
+using Movies.Contracts.Requests;
+using Refit;
+
+var moviesApi = RestService.For<IMoviesApi>("https://localhost:5001");
+
+var movie = await moviesApi.GetMovieAsync("street-fighter-1992");
+
+var request = new GetAllMoviesRequest
+{
+    Page = 1,
+    PageSize = 3
+};
+
+var movies = await moviesApi.GetMoviesAsync(request);
+
+foreach (var movieResponse in movies.Items)
+{
+    Console.WriteLine(JsonSerializer.Serialize(movieResponse));
+}
+
+
---
+++ Movies.Api.Sdk/IMoviesApi.cs
@@ -0,0 +1,14 @@
+﻿using Movies.Contracts.Requests;
+using Movies.Contracts.Responses;
+using Refit;
+
+namespace Movies.Api.Sdk;
+
+public interface IMoviesApi
+{
+    [Get(ApiEndpoints.Movies.Get)]
+    Task<MovieResponse> GetMovieAsync(string idOrSlug);
+
+    [Get(ApiEndpoints.Movies.GetAll)]
+    Task<MoviesResponse> GetMoviesAsync(GetAllMoviesRequest request);
+}
